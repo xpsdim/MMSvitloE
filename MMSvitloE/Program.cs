b549ab0b6dc15434a8ea9a8d9e4ba167a1959041
dima.sukhorukoff <dima.sukhorukoff@gmail.com>
2022-11-22 14:14:26 +0200

    Add project files.

---
+++ MMSvitloE/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace MMSvitloE
+{
+	class Program
+	{
+		public static ITelegramBotClient bot = null;
+		public static IConfigurationRoot configuration;
+
+		public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+		{
+			if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+			{
+				var message = update.Message;
+				if (message.Text.ToLower() == "/start")
+				{
+					var pingResult = new Utils().PingHost(configuration["ipToPing"]);
+					var msg = "Світла нема.";
+					if (pingResult)
+					{
+						msg = "Світло Є!";
+					}
+
+					await botClient.SendTextMessageAsync(message.Chat, msg);
+					return;
+				}
+			}
+		}
+
+		public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+		{
+			// Just write error to console
+			Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+		}
+
+		public static void InitConfiguration()
+		{
+			ServiceCollection serviceCollection = new ServiceCollection();
+			// Build configuration
+			configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+				.AddJsonFile("appsettings.json", false)
+				.Build();
+
+			// Add access to generic IConfigurationRoot
+			serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+			bot = new TelegramBotClient(configuration["botToken"]);
+		}
+
+
+		static void Main(string[] args)
+		{
+			InitConfiguration();
+			Console.WriteLine("Запущено бота " + bot.GetMeAsync().Result.FirstName);
+
+			var cts = new CancellationTokenSource();
+			var cancellationToken = cts.Token;
+			var receiverOptions = new ReceiverOptions
+			{
+				AllowedUpdates = { },
+			};
+			bot.StartReceiving(
+				HandleUpdateAsync,
+				HandleErrorAsync,
+				receiverOptions,
+				cancellationToken
+			);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ MMSvitloE/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.NetworkInformation;
+
+namespace MMSvitloE
+{
+	public class Utils
+	{
+		public bool PingHost(string ip)
+		{
+			//set the ping options, TTL 128
+			PingOptions pingOptions = new PingOptions(128, true);
+
+			//create a new ping instance
+			Ping ping = new Ping();
+
+			//32 byte buffer (create empty)
+			byte[] buffer = new byte[32];
+
+			var successCnt = 0;
+
+			//here we will ping the host 4 times (standard)
+			for (int i = 0; i < 4; i++)
+			{
+				PingReply pingReply = ping.Send(ip, 1000, buffer, pingOptions);
+
+				//make sure we dont have a null reply
+				if (!(pingReply == null))
+				{
+					switch (pingReply.Status)
+					{
+						case IPStatus.Success:
+							successCnt++;
+							break;
+						default:
+							break;
+					}
+				}
+			}
+
+			return successCnt > 0;
+		}
+	}
+}
